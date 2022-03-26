d7496adc85335b0a2cf1f65ab7237c52a4c4ea0b
mohammadreza babaei mosleh <mohammadrezababaeimosleh@gmail.com>
2022-03-26 07:40:09 -1000

    creating Node class(working)

--- include/bst.h
+++ include/bst.h
@@ -2,25 +2,48 @@
 #define BST_H
 
 #include <iostream>
+
 class BST
 {
 public:
     class Node;
+    Node*& get_root();
+
+    
 private:
     Node* root;
 };
 
-
 class BST::Node
 {
 public:
-	Node(int value, Node* left = nullptr, Node* right =nullptr);
-	Node();
-	Node(const Node& node);
+    Node(int value, Node* left = nullptr , Node* right = nullptr)
+    {
+        std::cout << "normal constructor" << std::endl;
+        this->value = value;
+        this->left = left;
+        this->right = right;
+    }
 
-	int value;
-	Node* left;
-	Node* right;
+
+    Node()
+        : Node(0, nullptr , nullptr)
+    {
+        std::cout << "defualt constructor" << std::endl;
+    }
+
+
+    Node(const Node& node)
+        : value{ node.value } 
+        , left{ node.left }
+        , right{ node.right }
+    {
+        std::cout << " copy constructor " << std::endl;
+    }
+
+    int value;
+    Node* left;
+    Node* right;
 };
 
 #endif //BST_H
--- src/bst.cpp
+++ src/bst.cpp
@@ -1,25 +1,7 @@
 #include "bst.h"
 
 
-BST::Node::Node(int value, Node* left, Node* right)
+BST::Node*& BST::get_root()
 {
-    std::cout << "normal constructor" << std::endl;
-    this->value = value;
-    this->left = left;
-    this->right = right;
+    return root ; 
 }
-
-BST::Node::Node()
-    : BST::Node(0, nullptr , nullptr)
-{
-    std::cout << "defualt constructor" << std::endl;
-}
-
-BST::Node::Node(const Node& node)
-    : value{ node.value} 
-    , left{ node.left }
-    , right{ node.right }
-{
-   std::cout << " copy constructor " << std::endl;
-}
-
